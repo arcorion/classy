987832a9d197f090aaaf76d87de21de5a253d6a6
Christopher Lee Crader <github@wherestarsend.com>
2025-04-29 17:52:08 -0700

    Play with class and instance variables
    These changes reflect that a class can have variables which are distinct from the instance variables created when a class object is instantiated. "count" and "type" for the Animals class are overridden by the "dogs" and "cats" instances.

--- animals.py
+++ animals.py
@@ -1,5 +1,8 @@
 class Animals:
 
+    count = 0
+    type = "gargoyle"
+
     def __init__(self, type="cat", count=10):
         self.type = type
         self.count = count
@@ -7,3 +10,7 @@ class Animals:
     def report(self, additional=0):
         total_animals = self.count + additional
         print(f"Between you and me, we have {total_animals} {self.type}s.")
+
+    def shadow_report(self, additional=0):
+        total_animals = self.count + additional
+        print(f"Between you and me, we have {total_animals} shadow {self.type}s.")
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from animals import Animals
 
 dogs = Animals("dog", 5)
-dogs.report()
+dogs.shadow_report()
 
 dogs.count = 10
 
-result = dogs.report
+result = dogs.shadow_report
 result(20)
 
+Animals.shadow_report(Animals)
+
+cats = Animals()
+
+cats.shadow_report()
+cats.report()
+
 Animals.report(dogs)
+Animals.report(cats)
+Animals.count = 100
+Animals.shadow_report(Animals)
+print(cats.count)
